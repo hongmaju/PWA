efcffa96c38dd1dde59e61c34e8bcc0f4aba51e5
LiKai <517412663@qq.com>
2018-05-14 21:13:44 +0800

    端口占用错误
     运行  node  Server.js

--- Server.js
+++ Server.js
@@ -12,23 +12,22 @@
 // console.log('Server running at http://127.0.0.1:8080/');
 
 let http=require("http"); //引入核心http模块
+let mime={
+    '.js':'application/javascript',
+    '.css':'text/css'
+}
 let fs=require("fs");
-let mime=require("mime"); //解决文件类型
-let url=require("url");
 //创建一个函数，req代表客户端，res代表服务器可写流
 let listener=(req,res)=>{
 //res是可写流，有write和end
-    let {query,pathname}=url.parse(req.url,true);
-    if(pathname==="/"){
+    if(req.url==="/"){
         //设置编码
         res.setHeader('Content-Type','text/html;charset=utf-8');
         fs.createReadStream('index.html').pipe(res);
     }else{
-        if(fs.existsSync(`.${pathname}`)) {
-            //mime 第三方包 npm install mime --save
-            //mime.lookup可以通过文件路径后缀判断是什么类型的
-            res.setHeader('Content-Type', mime.lookup(pathname)+';charset=utf-8');
-            fs.createReadStream(`.${pathname}`).pipe(res);
+        if(fs.existsSync(`${req.url}`)) {
+            res.setHeader('Content-Type',mime[req.url.match(/\.\w+$/)[0]] +';charset=utf-8');
+            fs.createReadStream(`.${req.url}`).pipe(res);
         }else{
             res.statusCode=404;
             res.end();
@@ -44,3 +43,5 @@ server.listen(port,function () {
     //启动成功后
     console.log(`start${port}`);
 })
+
+
